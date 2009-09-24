08e7aa8866d8721f0a8084aac7e271ab71c90651
darkseid <rafael.manoel@gmail.com>
2009-09-23 22:25:55 -0300

    a

--- app/helpers/scoring_helper.rb
+++ app/helpers/scoring_helper.rb
@@ -1,2 +1,11 @@
 module ScoringHelper
+  
+  def compute_scoring match    
+    if match.draw?
+      match.home_team.add_score 1
+      match.foreign_team.add_score 1
+    else
+      match.winner.add_score 3
+    end  
+  end  
 end
--- spec/models/scoring_spec.rb
+++ spec/models/scoring_spec.rb
@@ -1,21 +1,39 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe "ScoreComputing" do
+describe "Scoring" do
+  include ScoringHelper
 	
-	context "in a '3-1-0' system" do
+	context " '3-1-0' strategy" do
 		
-		specify "should give 3 points to the winner" do
-			
+		specify "should compute 3 points to the winner" do
 			m = Match.new({:home_team => Team.new({:name => 'corinthians'}), :foreign_team => Team.new({:name => 'santos'})})
 			m.home_team_score = 1
-			m.home_team_score = 0			
+			m.foreign_team_score = 0			
 			
-			ScoringHelper.compute_scoring m
+			compute_scoring m
 			
-			m.home_team.should have(3).points
-			m.foreign_team.should have(0).points
+			m.home_team.points.should == 3					
+		end
+		
+		specify "should compute 0 points to the looser" do
+			m = Match.new({:home_team => Team.new({:name => 'corinthians'}), :foreign_team => Team.new({:name => 'santos'})})
+			m.home_team_score = 1
+			m.foreign_team_score = 0			
 			
+			compute_scoring m
 			
+			m.foreign_team.points.should == 0
+		end
+
+		specify "should compute 1 point to both team when draw" do
+			m = Match.new({:home_team => Team.new({:name => 'corinthians'}), :foreign_team => Team.new({:name => 'santos'})})
+			m.home_team_score = 3
+			m.foreign_team_score = 3			
+			
+			compute_scoring m
+
+			m.home_team.points.should == 1			
+			m.foreign_team.points.should == 1
 		end
 		
 	end
