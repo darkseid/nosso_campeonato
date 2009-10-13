b3262d59ad4877f5b2bab6698e089ae9f0f035c9
darkseid <rafael.manoel@gmail.com>
2009-10-13 14:39:28 -0300

    novas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :phases
   map.resources :matches
+  map.resources :championships
+  map.resources :teams
 
   # The priority is based upon order of creation: first created -> highest priority.
 
